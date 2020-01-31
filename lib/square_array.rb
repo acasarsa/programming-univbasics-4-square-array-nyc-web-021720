3437be7726d67e7d607354d04c00b29e4c8d450b
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-31 23:39:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
  counter = 0
- newArray = []
  while counter < array.length do
-   a = array[counter]*= array[counter]
+   array[counter]*= array[counter]
    counter += 1
-   newArray.push(a)
  end
 end
