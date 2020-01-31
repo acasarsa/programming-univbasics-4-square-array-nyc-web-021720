c72dae0682b94a5273cf674c76ac525c944dd18e
Andrew J Casarsa <acasarsa@gmail.com>
2020-01-31 23:18:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
+# def output_array_elements(array)
+#  counter = 0
+#
+#  while counter < array.length do
+#    puts array[counter]**
+#    counter += 1
+#  end
+# end
+
 def square_array(array)
-  # your code here
+  i = 0
+  while i < array.length do
+    array[i] * array[i]
+  end
 end
